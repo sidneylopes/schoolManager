abf010672968b1f9bb7fa20d68e43a9b0093a0ae
sidneylopes <sidneylopesjr@gmail.com>
2021-06-11 17:46:26 -0300

    feat: add student routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
+import studentRoutes from "./studentRoutes";
 
 const routes = Router();
 
 routes.use(userRoutes);
+routes.use(studentRoutes);
 
 export default routes;
