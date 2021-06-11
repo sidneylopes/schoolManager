af2faa3fa56eb1b92575c039a03a6c309cfaee7b
sidneylopes <sidneylopesjr@gmail.com>
2021-06-11 16:48:20 -0300

    fix: database config

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
+import UsersService from "../services/UsersService";
 
 export const store = async (req: Request, res: Response) => {
-  res.json({
-    message: "oioi"
-  });
+
+  const { email, password, username, fullname } = req.body;
+
+  const usersService = new UsersService();
+  const user = await usersService.create(
+    email,
+    password,
+    username,
+    fullname
+  );
+
+  return res.status(200).json(user);
+
 }
 export const index = async (req: Request, res: Response) => {
-  res.json({
-    message: "oioi"
-  });
+  
+    return res.json({
+      message: "menor que 5"
+    });
+  
 }
