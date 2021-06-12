7e00a3acf9bbf9c72fc6e6939ad1c05fca012a93
sidneylopes <sidneylopesjr@gmail.com>
2021-06-12 11:34:40 -0300

    fix: credentials error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import "./database";
+import cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+  })
+);
 app.use(express.json());
 app.use(routes);
 
